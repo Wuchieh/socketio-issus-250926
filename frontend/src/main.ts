b20181c198548b252ee05c4470e7706cdcbf7e34
Wuchieh <q20001116@gmail.com>
2025-09-26 21:08:24 +0800

    feat: add frontend template

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,24 +1,31 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import {io} from 'socket.io-client'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const ws1 = io('http://192.168.213.13:9090', {path: '/ws', autoConnect: false})
+const ws2 = io('http://192.168.213.13:9091', {path: '/ws', autoConnect: false})
+const ws3 = io('http://192.168.213.13:9092', {path: '/ws', autoConnect: false})
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const ws1Btn = document.querySelector("#ws1")! as HTMLButtonElement
+const ws2Btn = document.querySelector("#ws2")! as HTMLButtonElement
+const ws3Btn = document.querySelector("#ws3")! as HTMLButtonElement
+
+ws1.on('connect', () => {
+    ws1Btn.innerText = 'ws1 connected'
+})
+ws2.on('connect', () => {
+    ws2Btn.innerText = 'ws2 connected'
+})
+ws3.on('connect', () => {
+    ws3Btn.innerText = 'ws3 connected'
+})
+
+ws1Btn.addEventListener('click', () => {
+    ws1.connect()
+})
+
+ws2Btn.addEventListener('click', () => {
+    ws2.connect()
+})
+
+ws3Btn.addEventListener('click', () => {
+    ws3.connect()
+})
